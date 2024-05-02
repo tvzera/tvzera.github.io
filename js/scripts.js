28c999b545ac2d4c44e646336f0d0e83e2ec6851
Gabriel Watts <111545311+tvzera@users.noreply.github.com>
2024-05-02 19:54:52 -0300

    iniciando site

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+$(document).ready(function() {
+    $('.carousel').slick({
+        autoplay: true
+         // Adicione pontos de navegação se desejar
+        // Outros parâmetros personalizáveis
+    });
+});
